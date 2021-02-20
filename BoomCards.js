f548f258f84548fdb577e3238713fecd02562339
omahesh <46605803+omahesh@users.noreply.github.com>
2021-02-19 19:31:51 -0800

    Update BoomCards.js

--- BoomCards.js
+++ BoomCards.js
@@ -1,24 +1,27 @@
-let cardsLeft = parseInt(document.querySelector("span.playCards").innerText);;
-do {
-  await new Promise((resolve) => {
-    for (let i = 0; i < cardsLeft; i++) {
-      setTimeout(() => {
-        a = document.querySelector("#play-surrender");
-        a.click();
+async function main() {
+  let cardsLeft = parseInt(document.querySelector("span.playCards").innerText);
+  do {
+    await new Promise((resolve) => {
+      for (let i = 0; i < cardsLeft; i++) {
         setTimeout(() => {
-          b = document.querySelector("body > div > div.backdrop.visible.active > div > div > div.popup-buttons > button.button.button-positive");
-          b.click();
+          a = document.querySelector("#play-surrender");
+          a.click();
           setTimeout(() => {
-            c = document.querySelector("body > div > div.backdrop.visible.active > div > div > div.popup-buttons > button");
-            c.click();
-            cardsLeft = parseInt(document.querySelector("span.playCards").innerText);
-            resolve();
+            b = document.querySelector("body > div > div.backdrop.visible.active > div > div > div.popup-buttons > button.button.button-positive");
+            b.click();
+            setTimeout(() => {
+              c = document.querySelector("body > div > div.backdrop.visible.active > div > div > div.popup-buttons > button");
+              c.click();
+              cardsLeft = parseInt(document.querySelector("span.playCards").innerText);
+              resolve();
+            }, 1000);
           }, 1000);
         }, 1000);
-      }, 1000);
-    }
-  });
-} while (cardsLeft != 0)
-score = parseInt(prompt("What score do you want?"));
-document.querySelector("div.donePreviewDeckContent > h3").outerHTML = `<h3>End of Play</h3><br><p>You got ${score} correct!</p>`;
-document.querySelector("div.donePreviewDeckContent > br:nth-child(5)").remove();
+      }
+    });
+  } while (cardsLeft != 0)
+  score = parseInt(prompt("What score do you want?"));
+  document.querySelector("div.donePreviewDeckContent > h3").outerHTML = `<h3>End of Play</h3><br><p>You got ${score} correct!</p>`;
+  document.querySelector("div.donePreviewDeckContent > br:nth-child(5)").remove();
+}
+main();
