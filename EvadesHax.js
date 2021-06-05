0fe60f1f18d443196c8c488ea4c00e65b83913e6
lce4113 <46605803+lce4113@users.noreply.github.com>
2021-06-04 18:05:58 -0700

    Update EvadesHax.js

--- EvadesHax.js
+++ EvadesHax.js
@@ -1,20 +1,22 @@
 // javascript:void (document.head.appendChild(document.createElement('script')).src = "https://lce4113.github.io/public/EvadesHax.js")
 
-document.onkeydown = e => {
-    let c = document.querySelector("#canvas");
-    switch (e.key) {
-        case "-":
-            c.width += 500;
-            window.dispatchEvent(new Event('resize'));
-            break;
-        case "=":
-            if (c.width == 1280) return;
-            c.width -= 500;
-            window.dispatchEvent(new Event('resize'));
-            break;
-        case "0":
-            c.width = 1280;
-            window.dispatchEvent(new Event('resize'));
-            break;
-    }
-}
+(function main() {
+    document.onkeydown = e => {
+        let c = document.querySelector("#canvas");
+        switch (e.key) {
+            case "-":
+                c.width += 500;
+                window.dispatchEvent(new Event('resize'));
+                break;
+            case "=":
+                if (c.width == 1280) return;
+                c.width -= 500;
+                window.dispatchEvent(new Event('resize'));
+                break;
+            case "0":
+                c.width = 1280;
+                window.dispatchEvent(new Event('resize'));
+                break;
+        }
+    };
+})();
